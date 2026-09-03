37d505374ed76731fc8fc7e5c9a775535f31e2d1
nexbytetechnologies <nexbyte.cl@gmail.com>
2026-09-03 19:47:31 -0400

    chore: prepare Prisma config for production

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  datasource: {
+ datasource: {
   url: env("DATABASE_URL"),
-  shadowDatabaseUrl: env("SHADOW_DATABASE_URL"),
+  shadowDatabaseUrl: process.env.SHADOW_DATABASE_URL,
 },
 });
